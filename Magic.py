61df62be311cefea20ab3cb61466d777eb421e08
Martin <34022590+mmeest@users.noreply.github.com>
2021-02-13 13:47:49 +0200

    Update Magic.py

--- Magic.py
+++ Magic.py
@@ -21,9 +21,3 @@ for pdf in f:
 
 merger.write("Magic PDF/Magic PDF Joiner Result.pdf")
 merger.close()
-
-""" save_path = 'Magic PDF/'
-name_of_file = 'Macic Joiner Result.pdf'
-completeName = os.path.join(save_path, name_of_file)       
-file1 = open(completeName, "w")
-file1.close() """
